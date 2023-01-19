3d0489f7bce27c1fc9e2293a573a4f9656647bcd
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2023-01-19 19:57:34 -0300

    Feat: Created objects in src/index file to simulate battle

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Luffy');
+const player2 = new Character('Zoro');
+const player3 = new Character('Sanji');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster(77, 21);
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles([...battles]: Battle[]) {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+}
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
