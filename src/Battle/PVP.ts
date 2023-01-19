63f1829868753a04489c2c707c6b49e60092d1b7
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2023-01-19 01:02:16 -0300

    Feat: Created the PVP class

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,28 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+export default class PVP extends Battle {
+  private _playerOne: Fighter;
+  private _playerTwo: Fighter;
+
+  constructor(playerOne: Fighter, playerTwo: Fighter) {
+    super(playerOne)
+    this._playerOne = playerOne;
+    this._playerTwo = playerTwo;
+  }
+
+  public fight(): number {
+    let winner = 0;
+    const NO_LIFE_POINTS = -1;
+
+    while (this._playerOne.lifePoints > 0 && this._playerTwo.lifePoints > 0) {
+      this._playerOne.attack(this._playerTwo);
+      this._playerTwo.attack(this._playerOne);
+    }
+
+    if (this._playerOne.lifePoints === NO_LIFE_POINTS) winner = -1;
+    if (this._playerTwo.lifePoints === NO_LIFE_POINTS) winner = 1;
+
+    return winner;
+  }
+}
